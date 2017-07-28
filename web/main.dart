82b19f45d0b0751cad245595b218cc985e2bb6ad
Chase Hardin <chasehardin@Chases-MacBook-Pro.local>
2017-07-28 13:21:13 -0500

    Initial project setup. Added over_react dependencies.

---
+++ web/main.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+import 'package:react/react.dart' as react;
+import 'package:react/react_dom.dart' as react_dom;
+import 'package:over_react/over_react.dart';
+
+void main() {
+  print('yo dog!');
+}
